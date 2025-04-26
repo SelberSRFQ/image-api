681b12ddea587a842e22ba6544d2117a05409a4d
SelberSRFQ <lg.selber@gmail.com>
2025-04-26 10:49:44 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -35,5 +35,5 @@ def ocr_image():
 
     return jsonify({'texto_extraido': texto_final})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=10000)

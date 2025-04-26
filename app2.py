58240d50e04f903f6272f3965fd70f0490b28d47
SelberSRFQ <lg.selber@gmail.com>
2025-04-26 13:26:04 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,16 +18,19 @@ def ocr_image():
 
         file = request.files['file']
         in_memory_file = file.read()
+
         npimg = np.frombuffer(in_memory_file, np.uint8)
         image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
         if image is None:
-            return jsonify({'error': 'Arquivo inválido ou formato não suportado!'}), 400
+            raise Exception("Falha ao decodificar imagem. Provavelmente formato inválido ou falta de suporte a JPEG no servidor.")
 
         texto = pytesseract.image_to_string(image, lang='por')
 
         return jsonify({'texto_extraido': texto})
     
     except Exception as e:
+        print(f"Erro interno no OCR: {e}")  # ADICIONAR ESSA LINHA
         return jsonify({'error': f'Erro interno: {str(e)}'}), 500
+
 
